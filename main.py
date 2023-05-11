c0f77ebc162058b9ec5e24d828971e1c7ee03d67
Younes Mirmohammadi <mir.younes01@gmail.com>
2023-05-11 16:23:08 +0330

    add regex_input method to give input from user

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ def init_window() -> tkinter.Tk:
     return root_window
 
 
+def regex_input(root_window: tkinter.Tk) -> None:
+    regex_input_var = tkinter.StringVar()
+    regex_input_label = tkinter.Label(root_window, text='regex', font=('calibre', 10, 'bold'))
+    regex_input_entry = tkinter.Entry(root_window, textvariable=regex_input_var, font=('calibre', 10, 'normal'))
+    sub_btn = tkinter.Button(root_window, text='Submit', command=lambda: print(regex_input_var.get()))
+
+    regex_input_label.grid(row=0, column=0)
+    regex_input_entry.grid(row=0, column=1)
+    sub_btn.grid(row=2, column=1)
+
+
 if __name__ == '__main__':
     root_window: tkinter.Tk = init_window()
+
+    regex_input(root_window=root_window)
 
     root_window.mainloop()
