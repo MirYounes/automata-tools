cc1092362a140036afdc4270b68bd5603b55b423
Younes Mirmohammadi <mir.younes01@gmail.com>
2023-06-07 20:24:35 +0330

    add concat_nfa and normalize methods to Nfa class

--- nfa.py
+++ nfa.py
@@ -6,18 +6,18 @@ from schemas import Symbols
 
 
 class Nfa:
-    states: Set[uuid.UUID]
-    initial_state: uuid.UUID
-    final_states: Set[uuid.UUID]
-    transactions: Dict[uuid.UUID, Dict[str, Set[uuid.UUID]]]
+    states: Set[str]
+    initial_state: str
+    final_states: Set[str]
+    transactions: Dict[str, Dict[str, Set[str]]]
     alphabets: Set[str]
 
     def __init__(
         self,
-        states: Set[uuid.UUID],
-        initial_state: uuid.UUID,
-        final_states: Set[uuid.UUID],
-        transactions: Dict[uuid.UUID, Dict[str, Set[uuid.UUID]]],
+        states: Set[str],
+        initial_state: str,
+        final_states: Set[str],
+        transactions: Dict[str, Dict[str, Set[str]]],
         alphabets: Set[str]
     ) -> None:
         self.states = states
@@ -25,18 +25,6 @@ class Nfa:
         self.final_states = final_states
         self.transactions = transactions
         self.alphabets = alphabets
-
-    @classmethod
-    def init_nfa(cls, character: str) -> 'Nfa':
-        initial_state = uuid.uuid4()
-        final_state = uuid.uuid4()
-        return cls(
-            initial_state=initial_state,
-            final_states={final_state},
-            states={initial_state, final_state},
-            transactions={initial_state: {character: {final_state}}},
-            alphabets={character}
-        )
 
     def __repr__(self) -> str:
         return pprint.pformat({
@@ -47,20 +35,99 @@ class Nfa:
             "transactions": self.transactions
         })
 
-    @staticmethod
-    def add_concat_symbol(regex: str) -> str:
-        new_regex = ""
-        for current_char in regex:
-            if (len(new_regex) > 0):
-                prev_char = new_regex[-1]
-                if (
-                    (prev_char in [Symbols.CLOSE_PARENTHESIS, Symbols.ZERO_OR_MORE,
-                                   Symbols.ONE_OR_MORE, Symbols.ZERO_OR_ONE] or Symbols.is_alphabet(prev_char))
-                    and (current_char == Symbols.OPEN_PARENTHESIS or Symbols.is_alphabet(current_char))
-                ):
-                    new_regex += Symbols.CONCAT
-            new_regex += current_char
-        return new_regex
+    def normalize(self) -> None:
+        states_table: Dict[str, str] = {}
+
+        state_counter = 1
+        new_states: Set[str] = set()
+        new_final_states: Set[str] = set()
+        new_initial_state: str = ''
+        for state in self.states:
+            new_state = f"{Symbols.STATE_NAME_PREFIX}{state_counter}"
+            states_table[state] = new_state
+            new_states.add(new_state)
+            if state == self.initial_state:
+                new_initial_state = new_state
+            if state in self.final_states:
+                new_final_states.add(new_state)
+            state_counter += 1
+
+        new_transactions: Dict[str, Dict[str, Set[str]]] = {}
+        for state in self.transactions.keys():
+            new_state = states_table[state]
+            new_transactions[new_state] = {}
+            for alphabet in self.transactions[state].keys():
+                new_transactions[new_state][alphabet] = set()
+                for transactions_state in self.transactions[state][alphabet]:
+                    new_transactions[new_state][alphabet].add(states_table[transactions_state])
+
+        self.states = new_states
+        self.initial_state = new_initial_state
+        self.final_states = new_final_states
+        self.transactions = new_transactions
+
+    @classmethod
+    def init_nfa(cls, character: str) -> 'Nfa':
+        initial_state = str(uuid.uuid4())
+        final_state = str(uuid.uuid4())
+        return cls(
+            initial_state=initial_state,
+            final_states={final_state},
+            states={initial_state, final_state},
+            transactions={initial_state: {character: {final_state}}},
+            alphabets={character, Symbols.EPSILON}
+        )
+
+    @classmethod
+    def concat_nfa(cls, nfa1: 'Nfa', nfa2: 'Nfa') -> 'Nfa':
+        merge = False
+        if len(nfa1.final_states) == 1:
+            nfa1_final_state = list(nfa1.final_states)[0]
+            nfa2.states.remove(nfa2.initial_state)
+            nfa2.states.add(nfa1_final_state)
+
+            transactions = nfa2.transactions.pop(nfa2.initial_state)
+            nfa2.transactions[nfa1_final_state] = transactions
+
+            for state in nfa2.states:
+                for alphabet in nfa2.alphabets:
+                    if nfa2.transactions.get(state) and nfa2.initial_state in nfa2.transactions[state].get(
+                            alphabet, {}):
+                        nfa2.transactions[state][alphabet].remove(nfa2.initial_state)
+                        nfa2.transactions[state][alphabet].add(nfa1_final_state)
+
+            nfa2.initial_state = nfa1_final_state
+            merge = True
+
+        nfa = cls(
+            states=nfa1.states.union(nfa2.states),
+            initial_state=nfa1.initial_state,
+            final_states=nfa2.final_states,
+            alphabets=nfa1.alphabets.union(nfa2.alphabets),
+            transactions={}
+        )
+
+        for state in nfa.states:
+            if state in nfa1.states and state in nfa2.states:
+                transactions = {}
+                for alphabet in nfa.alphabets:
+                    nfa1_alphabet_transactions = nfa1.transactions[state].get(
+                        alphabet, set()) if nfa1.transactions.get(state) else set()
+                    nfa2_alphabet_transactions = nfa2.transactions[state].get(alphabet, set())
+                    if nfa1_alphabet_transactions or nfa2_alphabet_transactions:
+                        transactions[alphabet] = nfa1_alphabet_transactions.union(
+                            nfa2_alphabet_transactions) if nfa2.transactions.get(state) else set()
+                nfa.transactions[state] = transactions
+            elif state in nfa1.states and nfa1.transactions.get(state):
+                nfa.transactions[state] = nfa1.transactions[state]
+            elif state in nfa2.states and nfa2.transactions.get(state):
+                nfa.transactions[state] = nfa2.transactions[state]
+
+        if not merge:
+            for state in nfa1.final_states:
+                nfa.transactions[state][Symbols.EPSILON].add(nfa2.initial_state)
+
+        return nfa
 
     @classmethod
     def regex_to_postfix(cls, regex: str) -> str:
@@ -96,3 +163,18 @@ class Nfa:
             postfix_regex += operator_stack.pop()
 
         return postfix_regex
+
+    @staticmethod
+    def add_concat_symbol(regex: str) -> str:
+        new_regex = ""
+        for current_char in regex:
+            if (len(new_regex) > 0):
+                prev_char = new_regex[-1]
+                if (
+                    (prev_char in [Symbols.CLOSE_PARENTHESIS, Symbols.ZERO_OR_MORE,
+                                   Symbols.ONE_OR_MORE, Symbols.ZERO_OR_ONE] or Symbols.is_alphabet(prev_char))
+                    and (current_char == Symbols.OPEN_PARENTHESIS or Symbols.is_alphabet(current_char))
+                ):
+                    new_regex += Symbols.CONCAT
+            new_regex += current_char
+        return new_regex
